26a8b6978931e157294f7f187143e8fa0ea9a174
Naheen Habib <naheenhabib@outlook.com>
2023-02-02 17:47:28 +1100

    new commit

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <a href="index.html">Home</a>
+            <a href="properties.php">Properties</a>
+            <a href="bookings.php">Bookings</a>
+            <a href="profile.php">My Profile</a>
+            <a href="register.php">Register</a>
+            <a href="login.php">Login</a>
+        </nav>
+    </header>
+    <section>
+        <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+
+        </form>
+    </section>
+</body>
+
+</html>
