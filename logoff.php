df65d6e774776ae9b2103119b067a3e30b698661
Naheen Habib <naheenhabib@outlook.com>
2023-02-10 14:06:38 +1100

    new commit

--- logoff.php
+++ logoff.php
@@ -1,10 +1,6 @@
 <?php
-session_start();
-
-// Destroy all data associated with the current session
 session_destroy();
 
-// Redirect the user to the home page
 header('Location: index.php');
 exit;
 ?>
